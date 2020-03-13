09b0f041b43c19447a608cbf35a1a8327a52b001
artie-sh <artie.sh.87@gmail.com>
2020-03-13 16:04:33 +0100

    google test

---
+++ src/test/java/blackswan/shendrikov/test/GoogleTest.java
@@ -0,0 +1,17 @@
+package blackswan.shendrikov.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+import pages.GetStartedPage;
+import pages.LogInPage;
+
+public class GoogleTest extends BaseTest {
+
+    @Test
+    public void logInAndSearchByCompanyNameTest() {
+        System.out.println(">>> starting logInAndSearchByCompanyNameTest");
+        driver.get("https://www.google.pl/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title='Search']")));
+    }
+}
